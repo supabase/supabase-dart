30eac0c145f4e5de6c7b2c40c3813618c4cc520b
phamhieu <phamhieu1998@gmail.com>
2021-02-04 18:18:08 +0800

    chore: update example

--- example/main.dart
+++ example/main.dart
@@ -4,6 +4,8 @@ void main() async {
   const supabaseUrl = '';
   const supabaseKey = '';
   final client = SupabaseClient(supabaseUrl, supabaseKey);
+
+  // query data
   final response = await client
       .from('countries')
       .select()
@@ -12,4 +14,23 @@ void main() async {
   if (response.error == null) {
     print('response.data: ${response.data}');
   }
+
+  // realtime
+  final supscription1 =
+      client.from('countries').on(SupabaseEventTypes.delete, (res) {
+    print('on countries.delete: ${res.table} ${res.eventType} ${res.olds}');
+  }).subscribe((String event, {String errorMsg}) {
+    print('event: $event error: $errorMsg');
+  });
+
+  final supscription2 =
+      client.from('todos').on(SupabaseEventTypes.insert, (res) {
+    print('on todos.insert: ${res.table} ${res.eventType} ${res.news}');
+  }).subscribe((String event, {String errorMsg}) {
+    print('event: $event error: $errorMsg');
+  });
+
+  // remember to remove subscription
+  client.removeSubscription(supscription1);
+  client.removeSubscription(supscription2);
 }
