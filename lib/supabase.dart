03e2af20d0426e9e45b5c592371ecd9a662fdf9a
phamhieu <phamhieu1998@gmail.com>
2021-02-25 10:15:51 +0800

    chore: update lib description

--- lib/supabase.dart
+++ lib/supabase.dart
@@ -1,3 +1,9 @@
+/// A dart client for Supabase. It supports database query, authenticate users
+/// and listen for realtime changes. This client makes it simple for developers
+/// to build secure and scalable products.
+///
+library supabase;
+
 export 'package:realtime_client/src/realtime_subscription.dart'
     show RealtimeSubscription;
 
