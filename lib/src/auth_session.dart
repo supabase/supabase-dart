335b49276d871ec442d3bb62d8c77c440a15b18b
Tyler <18113850+dshukertjr@users.noreply.github.com>
2021-04-08 19:35:28 +0900

    fix: aligned exports with supabase-js (#14)

---
+++ lib/src/auth_session.dart
@@ -0,0 +1,17 @@
+import 'package:gotrue/gotrue.dart' show Session, User;
+
+class AuthSession extends Session {
+  AuthSession(
+      {required String accessToken,
+      int? expiresIn,
+      String? refreshToken,
+      String? tokenType,
+      User? user})
+      : super(
+          accessToken: accessToken,
+          expiresIn: expiresIn,
+          refreshToken: refreshToken,
+          tokenType: tokenType,
+          user: user,
+        );
+}
