8136496c2a3aca19707fb719d0a75e05e7017ab2
Igor Kazhdan <igork3010@gmail.com>
2021-06-26 14:29:49 +0200

    export realtime_client
    Improve developer experience by not having manually import realtime_client since it's a transitive dependency

--- lib/supabase.dart
+++ lib/supabase.dart
@@ -4,6 +4,7 @@
 ///
 library supabase;
 
+export 'package:realtime_client/realtime_client.dart';
 export 'package:realtime_client/src/realtime_subscription.dart';
 export 'package:gotrue/gotrue.dart';
 export 'package:storage_client/storage_client.dart';
