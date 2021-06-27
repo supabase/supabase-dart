cd253990fad03dac883ca7de743977b3ceac148f
Igor Kazhdan <igork3010@gmail.com>
2021-06-27 13:35:49 +0200

    Remove realtime_subscription export
    This is not necessary anymore since we already export realtime_client
    
    Co-authored-by: Tyler <18113850+dshukertjr@users.noreply.github.com>

--- lib/supabase.dart
+++ lib/supabase.dart
@@ -5,7 +5,6 @@
 library supabase;
 
 export 'package:realtime_client/realtime_client.dart';
-export 'package:realtime_client/src/realtime_subscription.dart';
 export 'package:gotrue/gotrue.dart';
 export 'package:storage_client/storage_client.dart';
 export 'src/auth_session.dart';
