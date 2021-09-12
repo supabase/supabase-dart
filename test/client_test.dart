b2f47e4c58f3c733d5c3bfee8dae490935a53ccd
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2021-09-12 09:10:17 +0900

    chore: created constans file and tests for X-Client-Info header

--- test/client_test.dart
+++ test/client_test.dart
@@ -14,4 +14,28 @@ void main() {
     final builder = client.from('users').select();
     expect(builder.url.toString(), '/rest/v1/users?select=%2A');
   });
+
+  test('X-Client-Info header is set properly on auth', () {
+    final xClientHeaderBeforeSlash =
+        client.auth.api.headers['X-Client-Info']!.split('/').first;
+    expect(xClientHeaderBeforeSlash, 'supabase-dart');
+  });
+
+  test('X-Client-Info header is set properly on postgrest', () {
+    final xClientHeaderBeforeSlash =
+        client.from('cats').headers['X-Client-Info']!.split('/').first;
+    expect(xClientHeaderBeforeSlash, 'supabase-dart');
+  });
+
+  test('X-Client-Info header is set properly on realtime', () {
+    final xClientHeaderBeforeSlash =
+        client.realtime.headers['X-Client-Info']!.split('/').first;
+    expect(xClientHeaderBeforeSlash, 'supabase-dart');
+  });
+
+  test('X-Client-Info header is set properly on storage', () {
+    final xClientHeaderBeforeSlash =
+        client.storage.headers['X-Client-Info']!.split('/').first;
+    expect(xClientHeaderBeforeSlash, 'supabase-dart');
+  });
 }
