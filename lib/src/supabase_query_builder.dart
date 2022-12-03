40383136b4e9a8cfb4ada63494e853e4bb865866
daniel-hopkins <d@niestudio.com>
2022-12-03 08:45:54 -0500

    Update supabase_query_builder.dart (#166)
    * Update supabase_query_builder.dart
    
    Remove deprecated `execute()` from the docs on Streambuilder.
    
    * Update supabase_query_builder.dart
    
    Modified the new `.eq()` filter as described [here](https://supabase.com/docs/reference/dart/upgrade-guide#stream)
    
    * docs: update steam filter options
    
    Co-authored-by: Vinzent <vinzent03@proton.me>

--- lib/src/supabase_query_builder.dart
+++ lib/src/supabase_query_builder.dart
@@ -32,13 +32,13 @@ class SupabaseQueryBuilder extends PostgrestQueryBuilder {
   /// [primaryKey] list of name of primary key column(s).
   ///
   /// ```dart
-  /// supabase.from('chats').stream(primaryKey: ['my_primary_key']).execute().listen(_onChatsReceived);
+  /// supabase.from('chats').stream(primaryKey: ['my_primary_key']).listen(_onChatsReceived);
   /// ```
   ///
-  /// `eq`, `order`, `limit` filter are available to limit the data being queried.
+  /// `eq`, `neq`, `lt`, `lte`, `gt` or `gte` and `order`, `limit` filter are available to limit the data being queried.
   ///
   /// ```dart
-  /// supabase.from('chats:room_id=eq.123').stream(primaryKey: ['my_primary_key']).order('created_at').limit(20).execute().listen(_onChatsReceived);
+  /// supabase.from('chats').stream(primaryKey: ['my_primary_key']).eq('room_id','123').order('created_at').limit(20).listen(_onChatsReceived);
   /// ```
   SupabaseStreamBuilder stream({required List<String> primaryKey}) {
     assert(primaryKey.isNotEmpty, 'Please specify primary key column(s).');
